ef6b69cbf771bd40d48f3352cf60da535124bddd
fil7er <fil7er@gmail.com>
2022-06-08 12:32:47 -0400

    refac

--- src/example.mjs
+++ src/example.mjs
@@ -2,9 +2,7 @@ export class GameOfLife{
 
   createBoard(n) {
     let board = new Array;
-    for(let i = 0; i < n; i++){
-      board.push(new Array(n));
-    }
+    for(let i = 0; i < n; i++) board.push(new Array(n));
     for (let y = 0; y < n; y++){
       for (let x = 0; x < n; x++){
         board[y][x] = 'b';
