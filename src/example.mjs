7e1ac4b383249a9eec4943aba3f5a43b1d447239
fil7er <fil7er@gmail.com>
2022-06-08 14:01:47 -0400

    mak pass

--- src/example.mjs
+++ src/example.mjs
@@ -7,11 +7,15 @@ export class GameOfLife{
       this.board = new Board(size);
       this.createBoard();
       this.pattern = new Pattern(2,2, '2o$2o!');
+      this.patternOnBoard();
   }
 
   isClear = true;
   pattern;
   board;
+
+
+  //Board
 
   isBoardClear(){
     return this.isClear;
@@ -29,6 +33,7 @@ export class GameOfLife{
   }
 
 
+
   //Parsing
 
   parsePattern(){
@@ -41,6 +46,20 @@ export class GameOfLife{
     this.pattern.sizeX = parseInt(dataSplit.split("x =")[1].split(",")[0]);
     this.pattern.sizeY = parseInt(dataSplit.split("y =")[1].split(",")[0]);
   }
+
+
+  //Placing
+
+  patternOnBoard(){
+    let initX = Math.floor(this.board.size / 2) -1;
+    let initY = Math.floor(this.board.size / 2) -1;
+    for (let y = initY; y < this.pattern.sizeY + initY; y++){
+      for (let x = initX; x < this.pattern.sizeX + initX; x++){
+        this.board.display[y][x] = 'o';
+      }
+    }
+  }
+
 
 
   getPatternDimension(){
